a6aa7e3b1218fca0419390f18098d0370a415d1c
gggjhu2 <gggjhu2@gmail.com>
2020-08-07 15:35:34 +0900

    코배웹 시작하기 !

---
+++ ex00/src/main/java/org/zerock/sample/Restaurant.java
@@ -0,0 +1,19 @@
+package org.zerock.sample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+import lombok.Setter;
+
+@Component
+@Data
+        
+
+public class Restaurant {
+	
+	@Setter (onMethod_ = @Autowried)
+	private Chef chef;
+
+}
+   
